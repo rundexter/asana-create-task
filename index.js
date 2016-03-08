8fd9799f0e12cfdba17f2e8c72a9b86b1566a9dd
Daniel Ilkovich <daniel+github@betaworks.com>
2016-03-08 18:34:09 -0500

    Prepare for production

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+var _   = require('lodash')
+  , req = require('superagent')
+  , q   = require('q')
+;
+
 module.exports = {
     /**
      * The main entry point for the Dexter module
@@ -6,8 +11,59 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var workspace   = step.input('workspace').toArray()
+          , project     = step.input('project').toArray()
+          , credentials = dexter.provider('asana').credentials('access_token')
+          , followers   = step.input('followers').toArray()
+          , assignee    = step.input('assignee').toArray()
+          , notes       = step.input('notes').toArray()
+          , external_id = step.input( 'external_id' ).toArray()
+          , names       = step.input('name')
+          , promises    = []
+          , data        = {}
+        ;
+
+
+        names.each(function(name, idx) {
+            var data = { name: name }
+              , request = req.post('https://app.asana.com/api/1.0/tasks')
+                            .set('Authorization', 'Bearer '+credentials)
+                            .type('form')
+            ;
+
+            _.each(['assignee', 'notes', 'followers', 'workspace', 'project', 'external_id'], function(key) {
+                //try the on index item, if not just use the first item
+                setIfExists(data, key, step.input(key)[idx] || step.input(key).first());
+            });
+
+            request = request.send(data);
+
+            promises.push(
+                promisify(request, 'end', 'body.data')
+            );
+        });
+
+        q.all(promises)
+          .then(this.complete.bind(this))
+          .catch(this.fail.bind(this));
     }
 };
+
+function promisify(scope, call, path) {
+    var deferred = q.defer(); 
+
+    scope[call](function(err, result) {
+        return err
+          ? deferred.reject(err)
+          : deferred.resolve(_.get(result, path))
+        ;
+    });
+
+    return deferred.promise;
+}
+
+function setIfExists(obj, key, value) {
+    if(value !== null & value !== undefined) {
+        obj[key] = value;
+    }
+}
